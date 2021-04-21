62fb00ba56b6115741c9fe8be99ce82eecf7f5fe
mework <canmeng_web@qq.com>
2021-04-21 19:28:20 +0800

    Add webpack development mode proxy option

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -5,7 +5,22 @@
 
 module.exports = {
   devServer: {
+    // 热更新
     hot: true,
+
+    // 运行后自动打开浏览器
     open: true,
+
+    // proxy代理接口
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8080',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '',
+        },
+      },
+    },
   },
 }
