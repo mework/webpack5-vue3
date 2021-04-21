28fc160cb37a4f6e91fa609fcf6790df2e891ea6
mework <canmeng_web@qq.com>
2021-04-21 16:03:25 +0800

    Add webpack plugins "clean-webpack-plugin"

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,3 +1,13 @@
 /**
  * @description webpack生产环境配置
  */
+
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    new CleanWebpackPlugin(),
+  ]
+}
