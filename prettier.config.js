95125b6f67a060a8c1119674321e74ec357f388f
mework <canmeng_web@qq.com>
2021-04-22 16:47:58 +0800

    Add prettier.config.js file

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  printWidth: 120, // 行长规则通常设置为100或120
+  printWidth: 100, // 行长规则通常设置为100或120
   singleQuote: true, // 使用单引号, 默认false
   semi: true, // 使用分号, 默认true
   arrowParens: 'avoid', // 箭头函数参数添加括号
